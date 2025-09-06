e8bd3f64e13740a7d9b1bcef1ad4a3ce45d3d5ba
vster500 <levisbonventure@gmail.com>
2025-09-06 17:45:18 +0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBnpne91aBQsRYefwHO5fcaGliJTo8jhMk",
+    authDomain: "school-portal-233f5.firebaseapp.com",
+    projectId: "school-portal-233f5",
+    storageBucket: "school-portal-233f5.firebasestorage.app",
+    messagingSenderId: "314984934331",
+    appId: "1:314984934331:web:92922fcc7a959e1fa0729e"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
